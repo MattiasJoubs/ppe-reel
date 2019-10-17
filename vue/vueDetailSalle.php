1d4403267ba8b87afae872f0004da6c5645987fa
Mattias JOUBERT <mattias.joubert@icof.fr>
2019-10-17 17:12:07 +0200

    fin sprint 1

---
+++ vue/vueDetailSalle.php
@@ -0,0 +1,25 @@
+<h1>Liste des Salles Informatiques</h1>
+<?php
+for ($i = 0; $i < count($listeDetail); $i++) 
+{
+    if($listeDetail[$i]["nSalle"] == $idS){
+        ?>
+    
+        <div class="descrCard"><?= "<h2 class='titreSalle'>".$listeDetail[$i]['nomSalle'] . "</h2>"; ?>
+            <br />
+            <?= "Il y a ",$listeDetail[$i]["nbPoste"], " postes dans la salle" ?>
+            <br>
+            <?php for ($j = 0; $j < count($listeDetailPoste); $j++) {
+               if($listeDetail[$i]["nomSalle"] == $listeDetailPoste[$j]["nomSalle"]) {?>
+                 <br/><br/><h3><?= " ",$listeDetailPoste[$j]["nomPoste"]," :"; ?></h3> <br/>
+                <?= "Numéro De Poste : ",$listeDetailPoste[$j]["nPoste"];?> <br/>
+                <?= "ID Administrateur : ",$listeDetailPoste[$j]["ad"];?> <br/>
+                <?= "IP : ",$listeDetailPoste[$j]["indIP"];?> <br/>
+                <?= "Type Poste : ",$listeDetailPoste[$j]["typePoste"];
+            }}?>
+            <br />
+        </div>
+        <?php
+    }
+}
+?>
