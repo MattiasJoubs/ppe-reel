7c2dbf51e7d5ade201966acac1cc36b50bce3f49
Mattias JOUBERT <mattias.joubert@icof.fr>
2019-12-03 11:48:16 +0100

    Fix css

--- vue/vueDetailSalle.php
+++ vue/vueDetailSalle.php
@@ -6,13 +6,13 @@ for ($i = 0; $i < count($listeDetail); $i++)
     if($listeDetail[$i]["nSalle"] == $idS){
         ?>
     
-        <div class="descrCard"><?= "<h2 class='h2'>".$listeDetail[$i]['nomSalle'] . "</h2>"; ?>
+    <div class="alert alert-info" role="alert"><?= "<h2 class='h2bleu'>".$listeDetail[$i]['nomSalle'] . "</h2>"; ?>
             <br />
             <?= " Il y a ",$listeDetail[$i]["nbPoste"], " postes dans la salle" ?>
             <br>
             <?php for ($j = 0; $j < count($listeDetailPoste); $j++) {
                if($listeDetail[$i]["nomSalle"] == $listeDetailPoste[$j]["nomSalle"]) {?>
-                 <br/><br/><h3 class="h3"><?= " ",$listeDetailPoste[$j]["nomPoste"]," :"; ?></h3> <br/>
+                 <br/><br/><h3 class="h3bleu"><?= " ",$listeDetailPoste[$j]["nomPoste"]," :"; ?></h3> <br/>
                 <?= "Numéro De Poste : ",$listeDetailPoste[$j]["nPoste"];?> <br/>
                 <?= "IP : ",$listeDetailPoste[$j]["indIP"],".",$listeDetailPoste[$j]["ad"];?> <br/>
                 <?= "Type Poste : ",$listeDetailPoste[$j]["typePoste"];
