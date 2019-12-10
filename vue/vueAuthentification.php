e899d2c4e994773c7ee37c9ca5ae61840e3a45f0
Mattias JOUBERT <mattias.joubert@icof.fr>
2019-12-10 15:31:25 +0100

    fix

--- vue/vueAuthentification.php
+++ vue/vueAuthentification.php
@@ -5,7 +5,7 @@
 <form action="./?action=connexion" method="POST">
 <div> <img class="fit-picture"
      src="./images/m2l.png"> </div>
-<div class="form-group">
+<div class="form-group" id="form-connexion">
     <input type="text" class="form-control" name="mailU" placeholder="Email de connexion" /><br />
     <input type="password" class="form-control" name="mdpU" placeholder="Mot de passe"  /><br />
     <input type="submit" value="Se connecter" class="btn btn-success"/>
