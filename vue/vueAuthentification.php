aa10ddc664be80e2ba31013195a481fdaf7e93a9
Mattias JOUBERT <mattias.joubert@icof.fr>
2019-12-10 15:29:41 +0100

    new picture

--- vue/vueAuthentification.php
+++ vue/vueAuthentification.php
@@ -3,11 +3,14 @@
 <br>
 <br>
 <form action="./?action=connexion" method="POST">
+<div> <img class="fit-picture"
+     src="./images/m2l.png"> </div>
 <div class="form-group">
     <input type="text" class="form-control" name="mailU" placeholder="Email de connexion" /><br />
     <input type="password" class="form-control" name="mdpU" placeholder="Mot de passe"  /><br />
     <input type="submit" value="Se connecter" class="btn btn-success"/>
     <a href="./?action=inscription" class="btn btn-info">Inscription</a>
 </div>
+
 </form>
 
