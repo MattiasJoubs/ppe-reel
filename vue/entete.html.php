b2a2236868266320b2f97ab21a1f539d75b65f6a
Mattias JOUBERT <mattias.joubert@icof.fr>
2019-12-10 14:24:28 +0100

    background color

--- vue/entete.html.php
+++ vue/entete.html.php
@@ -16,7 +16,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     </head>
-    <body>
+    <body class="bleuciel">
+    <?php if(isLoggedOn()){ ?>
     <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary">
         <a class="navbar-brand" href="./?action=accueil"><img src="./images/logoBarre.png"/>M2L</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -34,17 +35,18 @@
         </div>
         <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
             <ul class="navbar-nav ml-auto">
-            <?php if(isLoggedOn()){ ?>
+            
             <li class="nav-item active"><a class="nav-link" href="./?action=profil">Mon Profil</a></li>
-            <?php } 
-            else{ ?>
+            
             <li class="nav-item active"><a class="nav-link" href="./?action=connexion">Connexion</a></li>
-            <?php } ?>
+            
             <li class="nav-item active">
                 <a class="nav-link" href="./?action=inscription">Inscription</a>
             </li>
             </ul>
         </div>
     </nav>
-
+    <?php } 
+            else{ ?>
+            <?php } ?>
     <div id="corps">
